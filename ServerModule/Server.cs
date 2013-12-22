799851f401574003e832ef74bd3f38903fa8a4d6
gmiejski <grz.miejski@gmail.com>
2013-12-22 16:04:16 +0100

    Player registration added

---
+++ ServerModule/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfacesModule;
+
+namespace ServerModule
+{
+    public class Server : IServer
+    {
+
+        private readonly Dictionary<string, IGameController> _activeGames;
+        private readonly Dictionary<string, CreatedGame> _availableGames;
+        private readonly Dictionary<string, string> _loggedPlayers;
+
+
+        public Server()
+        {
+            _activeGames = new Dictionary<string, IGameController>();
+            _availableGames = new Dictionary<string, CreatedGame>();
+            _loggedPlayers = new Dictionary<string, string>();
+        }
+
+        public bool CreateGame(string playerName, string gameName, GameType gameType, int numberOfPlayers, int numberOfBots, BotLevel botLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGame(string gameName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool JoinGame(string playerName, string gameName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeMove(string playerName, string gameName, Move move)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterPlayer(String playerName, string contextId)
+        {
+            if (String.IsNullOrEmpty(contextId) || String.IsNullOrEmpty(playerName))
+            {
+                return false;
+            }
+
+            if (_loggedPlayers.ContainsKey(playerName))
+            {
+                return false;
+            }
+            else
+            {
+                _loggedPlayers.Add(playerName, contextId);
+                return true;
+            }
+        }
+
+        public void UnregisterPlayer(string playerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CreatedGame> GetAvailableGames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameState GetGameState(string gameName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServerTestModule/ServerTest.cs
@@ -0,0 +1,54 @@
+﻿using CommonInterfacesModule;
+using NUnit.Framework;
+using ServerModule;
+
+namespace ServerTestModule
+{
+    [TestFixture]
+    public class ServerTest
+    {
+
+        private IServer _instance;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _instance = new Server();
+        }
+
+        [Test]
+        public void ShouldRegisterPlayer()
+        {
+            string playerName = "playerTestName";
+            string contextId = "context";
+            Assert.True(_instance.RegisterPlayer(playerName, contextId));
+        }
+
+        [Test]
+        public void ShouldNotregisterPlayerWhenEmptyArgumentString()
+        {
+            string playerName = "playerName";
+            string contextId = "context";
+            Assert.False(_instance.RegisterPlayer(playerName, ""));
+            Assert.False(_instance.RegisterPlayer("", contextId));
+        }
+
+        [Test]
+        public void ShouldNotRegisterPlayerWhenArgumentIsNull()
+        {
+            string contextId = "someContext";
+            string playerName = "playerName";
+            Assert.False(_instance.RegisterPlayer(null, contextId));
+            Assert.False(_instance.RegisterPlayer(playerName, null));
+        }
+
+        [Test]
+        public void ShouldNotRegisterPlayerWhenDesiredNameIsInUse()
+        {
+            string playerName = "playerName";
+            string contextId = "context";
+            Assert.True(_instance.RegisterPlayer(playerName, contextId));
+            Assert.False(_instance.RegisterPlayer(playerName, contextId));
+        }
+    }
+}
