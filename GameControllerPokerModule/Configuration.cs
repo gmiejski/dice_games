61924f922d33deb2bb429f8f885ae61f2cd167ce
Jakub Łasisz <jakublasisz@gmail.com>
2014-01-05 21:57:19 +0100

    Klasy Configuration i Hands

---
+++ GameControllerPokerModule/Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dice.GameControllerPokerModule
+{
+    class Configuration
+    {
+        private Hands _hands;
+        private int _higherValue;
+        private int _lowerValue;
+
+        public Configuration(Hands hands, int higherValue, int lowerValue = 0)
+        {
+            _hands = hands;
+            _higherValue = higherValue;
+            _lowerValue = lowerValue;
+        }
+
+        public Hands Hands
+        {
+            get { return _hands; }
+            set { _hands = value; }
+        }
+
+        public int HigherValue
+        {
+            get { return _higherValue; }
+            set { _higherValue = value; }
+        }
+
+        public int LowerValue
+        {
+            get { return _lowerValue; }
+            set { _lowerValue = value; }
+        }
+
+    }
+}
---
+++ GameControllerPokerModule/Hands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dice.GameControllerPokerModule
+{
+    enum Hands
+    {
+        HighCard,
+        Pair,
+        TwoPair,
+        Three,
+        Full,
+        Four,
+        Five
+    }
+}
