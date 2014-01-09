f257a007f659f55fae28669354f2c860e0c6e5bf
Jakub Mazurek <jmazurek@student.agh.edu.pl>
2014-01-09 01:30:18 +0100

    AbstractGameController and GameType created

---
+++ GameControllerNModule/AbstractGameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfacesModule;
+
+namespace GameControllerNModule
+{
+    abstract class AbstractGameController
+    {
+        protected List<IBot> _bots;
+        protected String _gameName;
+        protected GameType _gameType;
+        protected String _ownerName;
+        protected List<String> _playerNames;
+
+        protected AbstractGameController(List<IBot> bots, string gameName, string ownerName, GameType gameType, List<string> playerNames)
+        {
+            _bots = bots;
+            _gameName = gameName;
+            _ownerName = ownerName;
+            _gameType = gameType;
+            _playerNames = playerNames;
+        }
+    }
+}
