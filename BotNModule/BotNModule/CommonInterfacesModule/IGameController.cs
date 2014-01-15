11313821ee180620b10b9ee485085a7163097cb3
wbaszczyk <wbaszczyk@gmail.com>
2014-01-15 11:16:32 +0100

    Template project

---
+++ BotNModule/BotNModule/CommonInterfacesModule/IGameController.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CommonInterfacesModule
+{
+
+    public delegate void BroadcastGameStateHandler(string gameName, string gameState);
+    public delegate void DeleteGameControllerHandler (string gameName);
+
+    public interface IGameController
+    {
+        event BroadcastGameStateHandler BroadcastGameState;
+        event DeleteGameControllerHandler DeleteGameController;
+
+        GameState GameState { get; set; }
+
+        bool MakeMove(string playerName, Move move);
+
+    }
+}
