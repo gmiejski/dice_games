e15253c43add5fc12117835fbbe0b2ecdf7eca9c
Kamil Łukasz <klukasz@student.agh.edu.pl>
2014-01-16 00:18:31 +0100

    add tests class for NStarBot

--- BotNModule/BotNModule/NStarBotTest.cs
+++ BotNModule/BotNModule/NStarBotTest.cs
@@ -4,26 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BotNModule;
-using Moq;
 using CommonInterfacesModule;
 using NUnit.Framework;
+using Moq;
 
 namespace BotNTestModule
 {
     [TestFixture]
-    class BotNTest
+    class NStarBotTest
     {
-
-        private NPlusBot _botEasy;
-        private NPlusBot _botHard;
+        private NStarBot _botEasy;
+        private NStarBot _botHard;
         private const BotLevel LevelEasy = BotLevel.Easy;
         private const BotLevel LevelHard = BotLevel.Hard;
 
         [SetUp]
         public void SetUp()
         {
-            _botEasy = new NPlusBot(LevelEasy);
-            _botHard = new NPlusBot(LevelHard);
+            _botEasy = new NStarBot(LevelEasy);
+            _botHard = new NStarBot(LevelHard);
         }
 
         [Test]
@@ -36,25 +35,25 @@ namespace BotNTestModule
         [Test]
         public void TestUltimateProbality()
         {
-            List<int> dices = new List<int>(new int[] { 6, 6, 6, 6, 6 });
-            List<int> dicesToRoll = _botHard.UltimateProbability(dices, 30);
+            List<int> dices = new List<int>(new int[] { 1, 1, 1, 1, 1 });
+            List<int> dicesToRoll = _botHard.UltimateProbability(dices, 1);
             Assert.AreEqual(0, dicesToRoll.Count);
 
-            dices = new List<int>(new int[] { 6, 2, 6, 6, 6 });
-            dicesToRoll = _botHard.UltimateProbability(dices, 30);
+            dices = new List<int>(new int[] { 1, 1, 2, 1, 1 });
+            dicesToRoll = _botHard.UltimateProbability(dices, 1);
             Assert.AreEqual(1, dicesToRoll.Count);
             Assert.IsTrue(dicesToRoll.Contains(2));
 
-            dices = new List<int>(new int[] { 1, 2, 3, 4, 5 });
-            dicesToRoll = _botHard.UltimateProbability(dices, 30);
+            dices = new List<int>(new int[] { 6, 2, 3, 4, 5 });
+            dicesToRoll = _botHard.UltimateProbability(dices, 1);
             Assert.AreEqual(5, dicesToRoll.Count);
 
             dices = new List<int>(new int[] { 5, 1, 4, 2, 5 });
-            dicesToRoll = _botHard.UltimateProbability(dices, 5);
+            dicesToRoll = _botHard.UltimateProbability(dices, 1);
             Assert.AreEqual(4, dicesToRoll.Count);
 
             dices = new List<int>(new int[] { 5, 1, 4, 2, 5 });
-            dicesToRoll = _botHard.UltimateProbability(dices, 6);
+            dicesToRoll = _botHard.UltimateProbability(dices, 2);
             Assert.AreEqual(3, dicesToRoll.Count);
         }
 
@@ -64,11 +63,12 @@ namespace BotNTestModule
             Assert.AreEqual(1.0, _botHard.CollectSetSize(1, 4));
             Assert.AreEqual(0.0, _botHard.CollectSetSize(1, 7));
             Assert.AreEqual(0.0, _botHard.CollectSetSize(1, 0));
-            Assert.AreEqual(1.0, _botHard.CollectSetSize(5, 30));
-            Assert.AreEqual(0.0, _botHard.CollectSetSize(5, 31));
-            Assert.AreEqual(0.0, _botHard.CollectSetSize(5, 4));
-            Assert.AreEqual(6.0, _botHard.CollectSetSize(2, 7));
-            Assert.AreEqual(5.0, _botHard.CollectSetSize(2, 6));
+            Assert.AreEqual(1.0, _botHard.CollectSetSize(5, 7776));
+            Assert.AreEqual(0.0, _botHard.CollectSetSize(5, 7777));
+            Assert.AreEqual(1.0, _botHard.CollectSetSize(5, 1));
+            Assert.AreEqual(5.0, _botHard.CollectSetSize(5, 5));
+            Assert.AreEqual(25.0, _botHard.CollectSetSize(5, 6));
         }
+
     }
 }
