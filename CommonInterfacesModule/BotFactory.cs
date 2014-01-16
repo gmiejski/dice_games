ded639d3cabce4db6683d8b99ae5108434cbefef
wbaszczyk <wbaszczyk@gmail.com>
2014-01-16 14:12:59 +0100

    BotFactory implemented

--- CommonInterfacesModule/BotFactory.cs
+++ CommonInterfacesModule/BotFactory.cs
@@ -1,14 +1,28 @@
 ﻿namespace CommonInterfacesModule
 {
     using System.Collections.Generic;
+    using BotNModule;
+    using BotPokerModule;
 
     public class BotFactory
     {
+        private static int index = 0;
         public static List<IBot> CreateBots(BotLevel botLevel, GameType gameType, int numberOfBots)
         {
-            throw new System.NotImplementedException();
-        }
+            List<IBot> botList = new List<IBot>(numberOfBots);
 
+            if (gameType == GameType.NPlus)
+                for (int i = 0; i < numberOfBots; i++)
+                    botList[i] = new NPlusBot(botLevel,(index+i).ToString());
+            else if (gameType == GameType.NStar)
+                for (int i = 0; i < numberOfBots; i++)
+                    botList[i] = new NStarBot(botLevel, (index+i).ToString());
+            else if (gameType == GameType.Poker)
+              for (int i = 0; i < numberOfBots;i++)
+                   botList[i] = new BotPoker(botLevel, (index+i).ToString());
+            index += numberOfBots;
+            return botList;
+        }
         private BotFactory()
         {
         }
